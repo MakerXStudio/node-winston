ff2aa17f323cf70b105ef500c4a0e85ae6994405
Mickaël Derriey <mderriey@gmail.com>
2023-09-08 09:34:07 +0200

    Update `Logger` type to be closer to winston's `Logger` (#115)
    * Update `Logger` type to be closer to winston's `Logger`
    
    * Pick specific methods for the interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { format, Format } from 'logform'
-import { createLogger as winstonCreateLogger, LoggerOptions } from 'winston'
+import { createLogger as winstonCreateLogger, Logger as WinstonLogger, LoggerOptions } from 'winston'
 import * as Transport from 'winston-transport'
 import { Console, ConsoleTransportOptions } from 'winston/lib/winston/transports'
 import { omitFormat } from './omit-format'
@@ -10,14 +10,19 @@ import { serializableErrorReplacer } from './serialize-error'
 export * from './omit-format'
 export * from './omit-nil-format'
 
-export type Logger = {
-  child(options: Record<string, unknown>): Logger
-  error(message: string, ...optionalParams: unknown[]): void
-  warn(message: string, ...optionalParams: unknown[]): void
-  info(message: string, ...optionalParams: unknown[]): void
-  verbose(message: string, ...optionalParams: unknown[]): void
-  debug(message: string, ...optionalParams: unknown[]): void
-}
+export type Logger = Pick<
+  WinstonLogger,
+  // Supports child loggers
+  | 'child'
+  // Common levels
+  | 'debug'
+  | 'error'
+  | 'info'
+  | 'verbose'
+  | 'warn'
+  // General log method
+  | 'log'
+>
 
 export interface CreateLoggerOptions {
   consoleFormat?: 'pretty' | 'json'
